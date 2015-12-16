59f286f5d0de0b2672f1797fdee12b8149dc065e
lei7dover <limhoff@wastebits.com>
2015-12-16 16:03:00 -0500

    added create action to org

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -12,10 +12,19 @@ class OrganizationsController < ApplicationController
     @organization=Organization.new
   end
 
+  def create
+    @organization=Organization.new(organization_params)
+    if @organization.save!
+      redirect_to @organization
+    else
+      render :action => 'new'
+    end
+  end
+
   private
-  
+
   def organization_params
   params.require(:organization).permit(:logo, :street, :city, :state, :zip, :name)
-end
+  end
 
 end
