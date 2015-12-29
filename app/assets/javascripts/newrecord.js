cce3703d43619a572e74e21141eabd040e6d02c2
lei7dover <limhoff@wastebits.com>
2015-12-28 20:26:49 -0500

    refactored code

--- app/assets/javascripts/newrecord.js
+++ app/assets/javascripts/newrecord.js
@@ -1,27 +1,17 @@
 $(document).on('click', '#button', function(){
-var recordsform = $('.records-container').last().html();
+var recordsform = $('.records-container').html();
 var recordsformall = $('#all-container').html();
-var position = recordsform.indexOf("attributes_");
 var numar = recordsformall.match(/attributes_[0-9]/g);
 var numar_len = numar.length;
-var numar_len_s = "length=" + numar_len.toString();
-var test = numar[numar_len - 1];
-alert(numar_len_s);
-alert(test);
-var realmax = test.substring(11,12);
-alert(realmax);
+var maxnumstr = numar[numar_len - 1];
+var realmax = maxnumstr.substring(11,12);
 var maxnum = parseInt(realmax,10);
 var rege1 = "_0_";
 var changefrom = new RegExp(rege1,"g");
 var changefrom1 = new RegExp("[0]", "g");
 var maxnumpl1 = maxnum + 1 ;
 var changeto = "_" + maxnumpl1.toString() + "_";
-alert("changeto = "+changeto);
 var changeto1 = "[" + maxnumpl1.toString() + "]";
 recordsform = recordsform.replace(changefrom, changeto);
 recordsform = recordsform.replace(changefrom1 , changeto1);
-var alertstring = changefrom + "|" + changeto ;
-var alertstring2 = recordsform.substring(90, 120);
-alert(changefrom+"->"+changeto);
-alert(changefrom1+"->"+changeto1)
 $('.actions').before(recordsform)});
