9c09ee45dc139880337da5ad550774c4c32c1e66
lei7dover <limhoff@wastebits.com>
2016-01-02 22:31:35 -0500

    filtering dropdowns closer to complete

--- app/models/person.rb
+++ app/models/person.rb
@@ -11,17 +11,17 @@ class Person < ActiveRecord::Base
 
   scope :by_organization_filter, -> (org) {where("people.organization_id" => org)}
 
-  scope :records_sport_pluck, -> {joins(:records).pluck("Distinct records.sport")}
+  scope :records_sport_pluck, -> {joins(:records).pluck("Distinct records.sport").sort}
   scope :records_sport_filter, -> (sport) { joins(:records).where("records.sport" => sport) }
 
-  scope :people_last_name_pluck, -> {pluck("Distinct people.last_name")}
+  scope :people_last_name_pluck, -> {pluck("Distinct people.last_name").sort}
   scope :people_last_name_filter, -> (last_name) { where("people.last_name" => last_name) }
 
-  scope :people_class_of_pluck, -> {pluck("Distinct people.class_of")}
+  scope :people_class_of_pluck, -> {where("people.class_of IS NOT NULL").pluck("Distinct people.class_of").sort}
   scope :people_class_of_filter, -> (class_of) { where("people.class_of" => class_of) }
 
-
-  scope :records_all_state_1st_team, -> (all_state_1st_team) { where("records.all_state_1st_team" => all_state_1st_team) }
+  scope :records_all_state_1st_team_pluck, -> {joins(:records).pluck("Distinct records.all_state_1st_team")}
+  scope :records_all_state_1st_team_filter, -> (all_state_1st_team) { where("records.all_state_1st_team" => all_state_1st_team) }
   scope :records_all_state_2nd_team, -> (all_state_2nd_team) { where("records.all_state_2nd_team" => all_state_2nd_team) }
   scope :records_all_state_3rd_team, -> (all_state_3rd_team) { where("records.all_state_3rd_team" => all_state_3rd_team) }
   scope :records_hall_of_fame, -> (hall_of_fame) { where("records.hall_of_fame" => hall_of_fame) }
